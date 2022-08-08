fc20aada80da29f100c5ab9560f3947cf49526c3
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-09 00:02:13 +0200

    commit

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express();
-const routes = require('./routes/tasks')
+const tasks = require('./routes/tasks')
 
+//middleware that is helping us for getting json data
+app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.status(200).send('Hello :)')
 })
 
-app.use('/api/v1/tasks',routes)
+app.use('/api/v1/tasks',tasks)
 
 
 const port = 3000
