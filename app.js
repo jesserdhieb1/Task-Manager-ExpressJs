3a8a7aa34e89d53ba11886d0967da2fc132d8fcc
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-08 22:32:18 +0200

    commit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express=require('express')
-const app=express();
+const express = require('express')
+const app = express();
 
 app.get('/',(req,res)=>{
     res.status(200).send('Hello :)')
 })
-app.listen(3000,()=>{
-    console.log('Server connected on port 3000 ...')
+const port = 3000
+app.listen(port,()=>{
+    console.log(`Server connected on port ${port} ...`)
 })
