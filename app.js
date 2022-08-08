e660128c95703b8b46c43a5e79147b64ea877dce
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-08 22:25:37 +0200

    commit

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Task Manager App')
+const express=require('express')
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.status(200).send('Hello :)')
+})
+app.listen(3000,()=>{
+    console.log('Server connected on port 3000 ...')
+})
