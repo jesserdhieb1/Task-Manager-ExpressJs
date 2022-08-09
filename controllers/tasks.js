15c8a36977bb671bfb53b5aba584e909c5f5f240
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-09 22:33:29 +0200

    commit

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,12 +1,24 @@
 const Task = require('../models/task')
 
-const getAllTasks = (req,res)=>{
-    res.status(200).send('get all tasks :)');
+const getAllTasks = async (req,res)=>{
+    try {
+        const task = await Task.get({});
+        res.status(200).send('get all tasks :)');
+    }
+    catch (error){
+        res.status(500).json({msg:error})
+    }
+
 }
 
 const createTask = async (req,res)=>{
-    const task = await Task.create(req.body)
-    res.status(201).json({task});
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({task});
+    }
+    catch (error){
+        res.status(500).json({msg:error})
+    }
 }
 
 
