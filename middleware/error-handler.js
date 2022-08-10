40ed590bc0bedddb1c7cf8ce10795c2fa2c53a01
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-10 17:28:31 +0200

    commit

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,9 @@
+const {CustomError} =  require('../errors/custom-error')
+
 const errorHandlerMiddleware = (err,req,res,next)=>{
+    if(err instanceof CustomError){
+        return res.status(err.statusCode).json({error:err.message})
+    }
     return res.status(500).json({error:'there is an error'})
 }
 
