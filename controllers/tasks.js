357632a1f77d7634c1533e63e1a4e7c25c621ca7
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-10 02:18:24 +0200

    commit

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,26 +1,17 @@
 const Task = require('../models/task')
+const asyncWrapper = require('../middleware/async')
 
-const getAllTasks = async (req,res)=>{
-    try {
+const getAllTasks = asyncWrapper(async (req,res)=>{
         const task = await Task.find({});
-        res.status(200).json({task});}
-    catch (error){
-        res.status(500).json({msg:error})
-    }
-}
+        res.status(200).json({task});
+})
 
-const createTask = async (req,res)=>{
-    try {
+const createTask = asyncWrapper(async (req,res)=>{
         const task = await Task.create(req.body)
         res.status(201).json({task});
-    }
-    catch (error){
-        res.status(500).json({msg:error})
-    }
-}
+})
 
-const getTask = async (req,res)=>{
-    try{
+const getTask =asyncWrapper( async (req,res)=>{
         const {id:task_id}=req.params;
         const task = await Task.findOne({_id:task_id})
         // const task = await Task.findById(task_id)
@@ -28,14 +19,9 @@ const getTask = async (req,res)=>{
             return res.status(404).json({msg:'please verify your id'})
         }
         res.status(200).json({task})
-    }
-    catch (error){
-        res.status(500).json({msg:error})
-    }
-}
+})
 
-const deleteTask = async (req,res)=>{
-    try{
+const deleteTask =asyncWrapper( async (req,res)=>{
         const {id:task_id}=req.params
         const task = await Task.findOneAndDelete({_id:task_id})
         // const task = await Task.findByIdAndDelete(task_id)
@@ -43,14 +29,9 @@ const deleteTask = async (req,res)=>{
             return res.status(404).json({msg:'please verify your id'})
         }
         res.status(200).json({task})
-    }
-    catch (error){
-        res.status(500).json({msg:error})
-    }
-}
+})
 
-const updateTask = async (req,res)=>{
-    try{
+const updateTask =asyncWrapper( async (req,res)=>{
         const {id:task_id}  = req.params
         const task  = await Task.findOneAndUpdate({_id:task_id},req.body,{new:true,runValidators:true})
         // const task  = await Task.findByIdAndUpdate(task_id,req.body,{new:true,runValidators:true})
@@ -58,11 +39,7 @@ const updateTask = async (req,res)=>{
             return res.status(404).json({msg:'please verify your id'})
         }
         res.status(200).json({task})
-    }
-    catch (error){
-        res.status(500).json({msg:error})
-    }
-}
+})
 
 module.exports={
     getAllTasks,
